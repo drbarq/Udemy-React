da17f362a8caf06ca07dddccefc87f338de3d0f0
drbarq <J.Tustin@gmail.com>
2019-11-05 11:32:38 -0700

    adding in hooks

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -4,6 +4,7 @@ import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList'
 import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
+import useHttp from '../hooks/http'
 
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
@@ -18,25 +19,9 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 }
 
-const httpReducer = (curHttpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return {loading: true, error: null}
-    case 'RESPONSE':
-      return {...curHttpState, loading: false }
-    case 'ERROR':
-      return {loading: false, error: action.errorMessage}
-    case 'CLEAR':
-      return {...curHttpState, error: null}
-    default: 
-      throw new Error('Should not be reached!')
-  }
-}
-
-
 function Ingredients() {
   const [ userIngredients, dispatch ] = useReducer(ingredientReducer, [])
-  const [ httpState, dispatchHttp] = useReducer(httpReducer, {loading: false, error: null})
+  const { isLoading, error, data, sendRequest } = useHttp()
 
   useEffect(() => {
     console.log('Rendering Ingredients', userIngredients)
@@ -47,35 +32,31 @@ function Ingredients() {
   }, [])
 
   const addIngredientHandler = useCallback(ingredient => {
-    dispatchHttp({type: 'SEND'})
-    fetch('https://udemy-react-burgerhook.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => {
-      dispatchHttp({type:'RESPONSE'})
-      return response.json()
-    }).then(responseData => {
-      dispatch({type: 'ADD', ingredient: {id: responseData.name, ...ingredient }})
-    })      
+    // dispatchHttp({type: 'SEND'})
+    // fetch('https://udemy-react-burgerhook.firebaseio.com/ingredients.json', {
+    //   method: 'POST',
+    //   body: JSON.stringify(ingredient),
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    // .then(response => {
+    //   dispatchHttp({type:'RESPONSE'})
+    //   return response.json()
+    // })
+    // .then(responseData => {
+    //   dispatch({type: 'ADD', ingredient: {id: responseData.name, ...ingredient }})
+    // })      
   }, [])
 
   const removeIngredientHandler = useCallback(ingredientId => {
-    dispatchHttp({type: 'SEND'})
-    fetch(`https://udemy-react-burgerhook.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE'
-    }).then(response => {
-      dispatchHttp({type:'RESPONSE'})
-      dispatch({type: 'DELETE', id: ingredientId})
-    }).catch(error => {
-      dispatchHttp({type: 'ERROR', errorMessage: error.message})
-    })
-  }, [])
+    // dispatchHttp({type: 'SEND'})
+    sendRequest(`https://udemy-react-burgerhook.firebaseio.com/ingredients/${ingredientId}.json`, 'DELETE')
+  }, [sendRequest])
 
   const clearError = useCallback(() => {
-    dispatchHttp({type: 'CLEAR'})
+    // dispatchHttp({type: 'CLEAR'})
+    
   }, [])
 
   const ingredientList = useMemo(() => {
@@ -89,10 +70,10 @@ function Ingredients() {
 
   return (
     <div className="App">
-      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <IngredientForm 
         onAddIngredient={addIngredientHandler} 
-        loading={httpState.loading}
+        loading={isLoading}
       />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
