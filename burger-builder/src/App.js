d474337823c0e7b6920ee8570d4377c16346e307
drbarq <J.Tustin@gmail.com>
2019-11-06 09:11:37 -0700

    add in style

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import styles from './App.module.css';
-
+import Layout from '../src/components/Layout/Layout'
 
 class App extends Component {
   render() {
     return(
+      // <div className={styles["App"]}>
       <div>
-
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     )
   }
