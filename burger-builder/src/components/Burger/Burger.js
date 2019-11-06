4e2b9993c7f7a6a1ad4d348a3a77cd402055e340
drbarq <J.Tustin@gmail.com>
2019-11-06 10:29:00 -0700

    adding in burger builder

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,13 +4,18 @@ import styles from './burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = props => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {         // array with 2 elements
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            }) 
+        })
+
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+                {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
-
         </div>
     )
 }
