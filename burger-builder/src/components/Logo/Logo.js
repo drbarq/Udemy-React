262a8842af4c2d76abd6e1d6320b941d40b05ca4
drbarq <J.Tustin@gmail.com>
2019-11-06 14:50:04 -0700

    Addingin image

---
+++ burger-builder/src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import burgerLogo from '../../assets/Images/burger-logo.png'
+
+const logo = (props) => (
+    <div>
+        <img src={burgerLogo}/>
+    </div>
+)
+
+export default logo
