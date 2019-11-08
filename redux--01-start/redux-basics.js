fb519291da65529fa0f98d2e7ae1fb5b9402219a
drbarq <J.Tustin@gmail.com>
2019-11-08 14:15:20 -0700

    Adding in redux

---
+++ redux--01-start/redux-basics.js
@@ -0,0 +1,19 @@
+// node.js import statement
+const redux = require('redux')
+const createStore = redux.createStore
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer 
+const rootReducer = (state = initialState, action) => {
+    return state
+}
+
+//  Store 
+const store = createStore(rootReducer)
+console.log(store.getState())
+
+// Dispatcher 
+// Subscription 
