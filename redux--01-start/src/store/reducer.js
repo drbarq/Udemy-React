d7c07dca1e9454be54ca1ee78568ae3c885c4958
drbarq <J.Tustin@gmail.com>
2019-11-08 15:21:45 -0700

    Adding in reducer

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -3,27 +3,51 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+    switch ( action.type ) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'SUB':
+            return {
+                counter: state.counter - action.val
+            }
+        default:
+            return state
     }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + action.val
-        }
-    }
-    if (action.type === 'SUB') {
-        return {
-            counter: state.counter - action.val
-        }
-    }
-    return state
 }
 
 export default reducer
+
+
+
+
+    // if (action.type === 'INCREMENT') {
+    //     return {
+    //         counter: state.counter + 1
+    //     }
+    // }
+    // if (action.type === 'DECREMENT') {
+    //     return {
+    //         counter: state.counter - 1
+    //     }
+    // }
+    // if (action.type === 'ADD') {
+    //     return {
+    //         counter: state.counter + action.val
+    //     }
+    // }
+    // if (action.type === 'SUB') {
+    //     return {
+    //         counter: state.counter - action.val
+    //     }
+    // }
+    // return state
