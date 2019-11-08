ab27a037e076155e0b6a045661ebb30582e8938e
drbarq <J.Tustin@gmail.com>
2019-11-07 18:43:26 -0700

    adding in router

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import Button from '../../../components/UI/Button/Button'
+import styles from './ContactData.module.css'
 
 class ContactData extends Component {
     state = {
@@ -6,16 +8,20 @@ class ContactData extends Component {
         email: '',
         address: {
             street: '',
-            postal;Code: ''
+            postalCode: ''
         }
     }
 
     render() {
         return(
-            <div>
+            <div className={styles.ContactData}>
                 <h4>Enter Your Contact Data</h4>
                 <form>
-                    
+                    <input type="text" name="name" placeholder="Your name" />
+                    <input type="email" name="email" placeholder="Your email" />
+                    <input type="text" name="street" placeholder="Street" />
+                    <input type="text" name="postal" placeholder="Postal Code" />
+                    <Button btnType="Success">ORDER HERE</Button>
                 </form>
             </div>
         )
