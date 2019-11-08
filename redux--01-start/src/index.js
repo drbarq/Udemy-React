8ea1b1a821ffddf3029ef0aa734c073579236f8f
drbarq <J.Tustin@gmail.com>
2019-11-08 16:18:14 -0700

    refactoring dispatches

--- redux--01-start/src/index.js
+++ redux--01-start/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer'
+import countReducer from '../src/store/reducers/counter'
+import resultReducer from '../src/store/reducers/result'
+// import reducer from './store/reducer'
 
-const store = createStore(reducer)
+// const store = createStore(reducer)
+
+const rootReducer = combineReducers({
+    ctr: countReducer,
+    res: resultReducer
+})
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ redux--01-start/src/store/reducers/result.js
@@ -0,0 +1,26 @@
+import * as actionTypes from '../actions'
+
+const initialState = {
+    results: []
+}
+
+const resultReducer = (state = initialState, action) => {
+    switch ( action.type ) {
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: action.result})
+                // use concat so it returns a new array, push mutates the original array
+            }
+        case actionTypes.DELETE_RESULT:
+            const updatedArray = state.results.filter(result => result.id !== action.resultID)
+            return {
+                ...state,
+                results: updatedArray
+            }
+        default:
+            return state
+    }
+}
+
+export default resultReducer
