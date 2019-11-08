9659fc9f4d41549f9e346136e695886b52c2fa2c
drbarq <J.Tustin@gmail.com>
2019-11-08 15:37:26 -0700

    Adding in reducer

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,25 +1,45 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch ( action.type ) {
         case 'INCREMENT':
+            // dont do this, doesnt deeply clone
+            // const newState = Object.assign({}, state)
+            // newState.counter = state.counter + 1 
+            // return newState
             return {
+                ...state,
                 counter: state.counter + 1
             }
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.val
             }
         case 'SUB':
             return {
+                ...state,
                 counter: state.counter - action.val
             }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+                // use concat so it returns a new array, push mutates the original array
+            }
+        // case 'DELETE_RESULT':
+        //     return {
+        //         ...state,
+        //         results: 0
+        //     }
         default:
             return state
     }
