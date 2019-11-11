8f7e9f96a4ad84d8bab0cab56803dc6ced7857e2
drbarq <J.Tustin@gmail.com>
2019-11-11 09:48:39 -0700

    Adding in logout

--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Aux from '../Aux/Aux'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
@@ -23,10 +24,14 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <Toolbar 
+                    drawerToggleClicked={this.sideDrawerToggleHandler}
+                    isAuth={this.props.isAuthenticated}
+                />
                 <SideDrawer 
                     open={this.state.showSideDrawer} 
                     closed={this.sideDrawerClosedHandler}
+                    isAuth={this.props.isAuthenticated}
                 />
                 <main className={styles.Content}>
                     {this.props.children}
@@ -36,4 +41,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.token !== null 
+    }
+}
+
+export default connect(mapStateToProps)(Layout)
